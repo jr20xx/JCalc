0a44a009092d662137e847d611832f25d0945fe7
jr20xx <pprichard0322@gmail.com>
2025-03-11 23:42:02 -0400

    Moved the makeOperation() method to the AlgorithmImplementation class

--- jcalc/src/main/java/cu/lt/joe/jcalc/algorithms/AlgorithmImplementation.java
+++ jcalc/src/main/java/cu/lt/joe/jcalc/algorithms/AlgorithmImplementation.java
@@ -1,5 +1,12 @@
 package cu.lt.joe.jcalc.algorithms;
 
+import org.apache.commons.math3.util.FastMath;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import cu.lt.joe.jcalc.exceptions.InfiniteResultException;
+import cu.lt.joe.jcalc.exceptions.NotNumericResultException;
+import cu.lt.joe.jcalc.exceptions.UnregisteredOperationException;
+
 /**
  * This class contains the footprint for implementing the algorithms used in this library. Any method
  * defined here <b>must not be accessible</b> to any class that's not an algorithm implementation.
@@ -33,5 +40,41 @@ public class AlgorithmImplementation
     protected static boolean isNumber(String number)
     {
         return number.matches("^[+-]?(?:\\d+(?:\\.\\d*)?|\\.\\d+)(?:[eE][+-]?\\d+)?$");
+    }
+
+    /**
+     * Takes two operands and an operator to perform the required operation with those operands given
+     * a specific operator.
+     *
+     * @param secondOperand the second operand to perform the operation
+     * @param operator      the operator to define the operation that will be performed
+     * @param firstOperand  the first operand to perform the operation
+     * @return A {@code String} with the result of performing the specified operation with the given operands
+     * @throws UnregisteredOperationException when the operator is not registered
+     * @author <a href="https://github.com/jr20xx">jr20xx</a>
+     * @since 1.2.0
+     */
+    protected static BigDecimal makeOperation(BigDecimal secondOperand, String operator, BigDecimal firstOperand)
+    {
+        switch (operator)
+        {
+            case "+":
+                return firstOperand.add(secondOperand);
+            case "-":
+                return firstOperand.subtract(secondOperand);
+            case "*":
+                return firstOperand.multiply(secondOperand);
+            case "/":
+                return firstOperand.divide(secondOperand, 12, RoundingMode.HALF_UP);
+            case "^":
+                double result = FastMath.pow(firstOperand.doubleValue(), secondOperand.doubleValue());
+                if (Double.isNaN(result))
+                    throw new NotNumericResultException();
+                else if (Double.isInfinite(result))
+                    throw new InfiniteResultException();
+                return BigDecimal.valueOf(result);
+            default:
+                throw new UnregisteredOperationException("Not declared operation: " + operator);
+        }
     }
 }
