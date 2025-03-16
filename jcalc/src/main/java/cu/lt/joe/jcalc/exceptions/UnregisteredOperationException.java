e214b3f11961f178e29e38d9d4de793c34e43ff9
jr20xx <pprichard0322@gmail.com>
2025-03-15 23:08:45 -0400

    Removed unnecessary custom exception for operators not registered

--- jcalc/src/main/java/cu/lt/joe/jcalc/exceptions/UnregisteredOperationException.java
+++
@@ -1,9 +0,0 @@
-package cu.lt.joe.jcalc.exceptions;
-
-public class UnregisteredOperationException extends RuntimeException
-{
-    public UnregisteredOperationException(String x)
-    {
-        super(x);
-    }
-}
