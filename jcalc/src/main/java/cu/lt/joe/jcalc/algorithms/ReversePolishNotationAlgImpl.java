810f0068be31a382eef485599eebe605b6070b52
jr20xx <pprichard0322@gmail.com>
2025-03-16 00:28:33 -0400

    - Moved cleaning process to the "cleanMathExpression()" method of each algorithm implementation. - Made BigDecimal.ZERO the return value when a not registered operator is passed to the "makeOperation()" method. - Added "×" and "÷" in the group alternatives of the "isOperator()" method. - Updated code to directly add "0" to the Math expression when using Shunting Yard and the expression starts with "-". - Improved the "getItemsArray()" method in the ShuntingYardAlgImpl class to make more clear what to do when "+" and "-" are part of a number. - Fixed the behavior of the "getItemsArray()" method in the ShuntingYardAlgImpl class to fix its errors when handling "×" and "÷". - Updated the the "getOperatorPrecedence()" method in the ShuntingYardAlgImpl class to return -1 when passing a not registered operator. - Started jobs to improve the detection of syntax errors when solving Math expressions. - Simplified the definition of the JCalc class.

--- jcalc/src/main/java/cu/lt/joe/jcalc/algorithms/ReversePolishNotationAlgImpl.java
+++ jcalc/src/main/java/cu/lt/joe/jcalc/algorithms/ReversePolishNotationAlgImpl.java
@@ -3,6 +3,7 @@ package cu.lt.joe.jcalc.algorithms;
 import java.math.BigDecimal;
 import java.util.ArrayDeque;
 import cu.lt.joe.jcalc.JCalc;
+import cu.lt.joe.jcalc.exceptions.SyntaxErrorException;
 
 /**
  * This class contains a custom implementation of the Reverse Polish Notation algorithm. Any method
@@ -16,6 +17,31 @@ import cu.lt.joe.jcalc.JCalc;
 public class ReversePolishNotationAlgImpl extends AlgorithmImplementation
 {
     /**
+     * Takes a Math expression and returns a {@link String} containing the Math expression after
+     * applying some common fixes to it. It returns {@code null} if the provided expression is
+     * either {@code null} or empty.
+     *
+     * @param mathExpression the Math expression to clean
+     * @return A {@link String} containing the given Math expression with some fixes
+     * @author <a href="https://github.com/jr20xx">jr20xx</a>
+     * @since 1.2.3
+     */
+    private static String cleanMathExpression(String mathExpression)
+    {
+        if (mathExpression == null || mathExpression.isEmpty())
+            return null;
+        String cleanedMathExpression = mathExpression.matches(".*\\s+.*") ?
+                mathExpression.replaceAll("\\s+", " ").trim() : mathExpression;
+        if (cleanedMathExpression.isEmpty())
+            return null;
+        if (cleanedMathExpression.contains(";"))
+            cleanedMathExpression = cleanedMathExpression.replace(";", " ");
+        if (cleanedMathExpression.contains(","))
+            cleanedMathExpression = cleanedMathExpression.replace(",", ".");
+        return cleanedMathExpression;
+    }
+
+    /**
      * Takes a {@link String} object that contains the Math expression in Reverse Polish Notation, splits
      * it using whitespaces as separators, solves the expression and returns its result, as a {@link String}.
      *
@@ -26,21 +52,26 @@ public class ReversePolishNotationAlgImpl extends AlgorithmImplementation
      */
     public static String solveMathExpression(String mathExpression)
     {
-        ArrayDeque<BigDecimal> solution = new ArrayDeque<>();
-        for (String popped : mathExpression.split(" "))
-        {
-            if (popped.equals("×")) popped = "*";
-            else if (popped.equals("÷")) popped = "/";
+        String cleanedMathExpression = cleanMathExpression(mathExpression);
+        if (cleanedMathExpression == null) return null;
 
-            if (isOperator(popped))
+        ArrayDeque<BigDecimal> solution = new ArrayDeque<>();
+        for (String element : cleanedMathExpression.split(" "))
+        {
+            if (element.equals("×")) element = "*";
+            else if (element.equals("÷")) element = "/";
+
+            if (isOperator(element))
             {
                 BigDecimal number1 = solution.remove();
                 if (solution.isEmpty())
                     break;
-                solution.push(makeOperation(number1, popped, solution.remove()));
+                solution.push(makeOperation(number1, element, solution.remove()));
             }
+            else if (isNumber(element))
+                solution.push(new BigDecimal(element));
             else
-                solution.push(new BigDecimal(popped));
+                throw new SyntaxErrorException("Unexpected item `" + element + "` received while solving the expression");
         }
         return formatResult(solution.pop());
     }
