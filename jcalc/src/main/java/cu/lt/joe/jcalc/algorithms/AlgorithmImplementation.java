5be98bbdb5a1b6c74fe78e0e965e914d42f194c3
jr20xx <pprichard0322@gmail.com>
2025-03-19 00:20:24 -0400

    Started works to add factorial operator to the implementation of the Shunting Yard algorithm

--- jcalc/src/main/java/cu/lt/joe/jcalc/algorithms/AlgorithmImplementation.java
+++ jcalc/src/main/java/cu/lt/joe/jcalc/algorithms/AlgorithmImplementation.java
@@ -6,6 +6,7 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import cu.lt.joe.jcalc.exceptions.InfiniteResultException;
 import cu.lt.joe.jcalc.exceptions.NotNumericResultException;
+import cu.lt.joe.jcalc.exceptions.NumericalDomainErrorException;
 
 /**
  * This class contains the footprint for implementing the algorithms used in this library. Any method
@@ -30,6 +31,19 @@ public class AlgorithmImplementation
     }
 
     /**
+     * Checks when a given {@link String} is the factorial operator
+     *
+     * @param possibleFactorialOperator the {@link String} to check
+     * @return {@code true} or {@code false} when the {@link String} is the factorial operator or not
+     * @author <a href="https://github.com/jr20xx">jr20xx</a>
+     * @since 1.2.4
+     */
+    protected static boolean isFactorialOperator(String possibleFactorialOperator)
+    {
+        return possibleFactorialOperator.equals("!");
+    }
+
+    /**
      * Checks when a given String is a valid number using a regular expression.
      *
      * @param number String containing a number
@@ -49,7 +63,7 @@ public class AlgorithmImplementation
      * @param secondOperand the second operand to perform the operation
      * @param operator      the operator to define the operation that will be performed
      * @param firstOperand  the first operand to perform the operation
-     * @return A {@link String} with the result of performing the specified operation with the given operands
+     * @return A {@link BigDecimal} with the result of performing the specified operation with the given operands
      * @author <a href="https://github.com/jr20xx">jr20xx</a>
      * @since 1.2.0
      */
@@ -72,6 +86,33 @@ public class AlgorithmImplementation
                 else if (Double.isInfinite(result))
                     throw new InfiniteResultException();
                 return BigDecimal.valueOf(result);
+            default:
+                return BigDecimal.ZERO;
+        }
+    }
+
+    /**
+     * Takes an operand and a unary operator to perform the required operation over the given operand.
+     *
+     * @param operand  the operand to perform the required operation
+     * @param operator the unary operator to define the operation that will be performed
+     * @return A {@link BigDecimal} with the result of performing the specified operation
+     * @author <a href="https://github.com/jr20xx">jr20xx</a>
+     * @since 1.2.4
+     */
+    protected static BigDecimal makeUnaryOperation(BigDecimal operand, String operator)
+    {
+        switch (operator)
+        {
+            case "!":
+                if (operand.compareTo(BigDecimal.ZERO) < 0)
+                    throw new NumericalDomainErrorException("Factorial is not defined for negative numbers");
+                else if (!operand.remainder(BigDecimal.ONE).equals(BigDecimal.ZERO))
+                    throw new NumericalDomainErrorException("Factorial is not defined for non-integer numbers");
+                BigDecimal result = BigDecimal.ONE;
+                for (BigDecimal i = BigDecimal.ONE; i.compareTo(operand) <= 0; i = i.add(BigDecimal.ONE))
+                    result = result.multiply(i);
+                return result;
             default:
                 return BigDecimal.ZERO;
         }
