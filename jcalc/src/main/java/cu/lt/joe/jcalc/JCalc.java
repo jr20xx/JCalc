dad496de5b54918c401f5c6e26f9fe8d0eb7c4f5
jr20xx <pprichard0322@gmail.com>
2025-09-17 15:27:47 -0400

    Added the alternative method "JCalc.solveMathExpression(String)"

--- jcalc/src/main/java/cu/lt/joe/jcalc/JCalc.java
+++ jcalc/src/main/java/cu/lt/joe/jcalc/JCalc.java
@@ -17,10 +17,30 @@ import cu.lt.joe.jcalc.exceptions.NotNumericResultException;
 public class JCalc
 {
     /**
+     * Takes a Math expression and returns its result. If the expression is empty, {@code null} will
+     * be returned. If the expression contains any whitespace, they'll be ignored. If you wish to
+     * customize how Math expressions are treated, use the alternative method
+     * {@link #solveMathExpression(String, ConfigurationBuilder)}.
+     *
+     * @param mathExpression a {@link String} containing the Math expression to solve
+     * @return A {@link String} containing the result of solving the given Math expression or {@code null}
+     * if the given expression is empty
+     * @throws NotNumericResultException when a not numeric (NaN) value is obtained
+     * @throws InfiniteResultException   when an Infinite result is obtained
+     * @author <a href="https://github.com/jr20xx">jr20xx</a>
+     * @since 3.0.1
+     */
+    public static String solveMathExpression(String mathExpression)
+    {
+        return mathExpression == null ? null :
+                ShuntingYardAlgImpl.solveMathExpression(mathExpression, new ConfigurationBuilder());
+    }
+
+    /**
      * Takes a Math expression and returns its result using the parameters defined in the provided
-     * {@link ConfigurationBuilder} instance. If the expression is empty, {@code null} will be
-     * returned. If the expression contains any whitespace, they'll be ignored. The
-     * {@link ConfigurationBuilder} instance is used to customize the way the Math expressions are
+     * {@link ConfigurationBuilder} instance to process it. If the expression is empty, {@code null}
+     * will be returned. If the expression contains any whitespace, they'll be ignored. The settings
+     * in the {@link ConfigurationBuilder} instance are used to customize how Math expressions are
      * treated, defining the value of the precision used for the final result obtained after solving
      * a Math expression and, besides that, determining if the parentheses in the Math expression
      * should be automatically balanced and if the trigonometric functions will use radians or degrees
@@ -41,7 +61,6 @@ public class JCalc
     public static String solveMathExpression(String mathExpression, ConfigurationBuilder configurationBuilder)
     {
         return mathExpression == null ? null :
-                ShuntingYardAlgImpl.solveMathExpression(mathExpression, configurationBuilder == null ?
-                        new ConfigurationBuilder() : configurationBuilder);
+                ShuntingYardAlgImpl.solveMathExpression(mathExpression, configurationBuilder);
     }
 }
