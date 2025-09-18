6fa1b2b2c48f75dc87a60c7a61c98143843ccc9a
jr20xx <pprichard0322@gmail.com>
2025-09-17 21:27:36 -0400

    Added a test for the IllegalArgumentException

--- jcalc/src/test/java/cu/lt/joe/jcalc/JCalcTest.java
+++ jcalc/src/test/java/cu/lt/joe/jcalc/JCalcTest.java
@@ -1,11 +1,19 @@
 package cu.lt.joe.jcalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class JCalcTest
 {
+    @Test
+    void testIllegalArgumentException()
+    {
+        assertThrows(IllegalArgumentException.class, () -> JCalc.solveMathExpression(null, null));
+    }
+
     @ParameterizedTest
     @CsvFileSource(files = "src/test/resources/basic_math_expressions.csv")
     void solveBasicMathExpressions(String expression, String expectedResult)
